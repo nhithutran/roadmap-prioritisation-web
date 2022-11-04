c4aa3300d4481bc7c3bf056ed0916bbe696f3720
Anthony B Chung <99620815+anthonybchung@users.noreply.github.com>
2022-11-05 03:18:18 +1100

    can store response on localstorage

--- src/context/auth.context.js
+++ src/context/auth.context.js
@@ -3,19 +3,20 @@ import React from "react";
 export const authContext = createContext();
 
 export const authReducer = (state, action) => {
+  console.log("inside authReducer")
   switch (action.type) {
     case "LOGIN":
-      return { user: action.payLoad };
+      return { user: action.payload };
     case "LOGOUT":
       return { user: null };
     default:
-      return state;
+       return state
   }
 };
 
 export const AuthContextProvider = (PropTypes) => {
   const [state, dispatch] = useReducer(authReducer, { user: null });
-  console.log(`AuthContext State: ${state}`);
+  console.log("AuthContext State:", state);
 
   return (
     <authContext.Provider value={{ ...state, dispatch }}>
--- src/hooks/useSignUp.js
+++ src/hooks/useSignUp.js
@@ -1,28 +1,41 @@
 import { useState } from "react";
 import axios from "axios"
-//import { useAuthContext } from "./userAuthContext";
-//import {axiosInstance} from "./axios";
+import {useAuthContext} from "./useAuthContext"
 
 export const useSignUp = () => {
   const [signUpError, setSignUpError] = useState(null);
   const [isLoading, setIsLoading] = useState(null);
-  //const { dispatch } = useAuthContext;
+  const { dispatch } = useAuthContext;
 
   const signUp = async (firstName, lastName, email, password) => {
     setIsLoading(true);
     setSignUpError(null);
-    //const json = null;
 
     const axiosInstance = axios.create({baseURL: 'http://localhost:5000'})
     
     axiosInstance
       .post("/api/v1/auth/register", { firstName, lastName, password, email })
       .then(function (response) {
-        setIsLoading(false);
-        console.log(response.data);
+       // const token = response.data.token
+        const status = response.status
+        if(status === 200){
+         
+          //save to local storage
+          localStorage.setItem('user',response)
+          
+          //update autocontext
+          dispatch({type:"LOGIN",payload: response})
+         
+          setIsLoading(false);
+        }
+       
       })
       .catch(function (error) {
-        console.log(error.response.data);
+        if(status !== 200){
+          setSignUpError(error.response)
+        }
+        
+        setIsLoading(false)
       });
   };
   return { signUp, isLoading, signUpError };
