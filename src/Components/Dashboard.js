13e274cd5761702045c8ee774d8aef38445bbd6a
Anthony B Chung <99620815+anthonybchung@users.noreply.github.com>
2022-11-15 02:18:46 +1100

    api, and Dashboard api filtering

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -2,11 +2,10 @@ import React from "react";
 import styled from "styled-components";
 import { DataGrid } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";
-import { getInitiatives } from '../config/api';
-import { Container, Row, Button, Col, } from "react-bootstrap";
+import { getInitiatives } from "../config/api";
+import { Container, Row, Button, Col } from "react-bootstrap";
 import InitiativeTopPanel from "./InitiativeTopPanel";
-import { useParams } from "react-router-dom"
-
+import { useParams } from "react-router-dom";
 
 const Styles = styled.div`
   .d-inline mx-2 {
@@ -24,14 +23,17 @@ const Styles = styled.div`
 //   let { Object_id } = useParams();
 // };
 
-const columns = [ 
-  { 
-    field: 'ticket_id',
-    headerName: 'Ticket#', 
-    width: 80, 
-    renderCell: (obj) => <a href={`http://localhost:3000/initiatives/${obj.id}`}>{obj.value}</a> },
-  { field: 'initiative', headerName: 'Initiative', width: 200 },
-  { field: 'description', headerName: 'Description', width: 400 },
+const columns = [
+  {
+    field: "ticket_id",
+    headerName: "Ticket#",
+    width: 80,
+    renderCell: (obj) => (
+      <a href={`http://localhost:3000/initiatives/${obj.id}`}>{obj.value}</a>
+    ),
+  },
+  { field: "initiative", headerName: "Initiative", width: 200 },
+  { field: "description", headerName: "Description", width: 400 },
   {
     field: "submit_date",
     headerName: "Submit date",
@@ -49,78 +51,82 @@ const columns = [
 ];
 
 // Fetch and store data from MongoDB initiative table. setData to res.data so it can be rendered.
-function Dashboard () {
+function Dashboard() {
   const [query, setQuery] = useState("");
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchinitiatives = async () => {
       const res = await getInitiatives();
-      const resData = res.data
-      setData(resData|| []); // Ensure that data not null
+      console.log(res);
+      const resData = res.data;
+      setData(resData || []); // Ensure that data not null
     };
     fetchinitiatives();
   }, []);
 
-// hasMatch function matches query with fields in table
-const hasMatch = (field, query) => {
-  // Got TypeError: Cannot read properties of undefined (reading 'includes'). Provided fallback for fields.
-  return (field || "").includes(query.toLowerCase())
-}
+  // hasMatch function matches query with fields in table
+  const hasMatch = (field, query) => {
+    // Got TypeError: Cannot read properties of undefined (reading 'includes'). Provided fallback for fields.
+    return (field || "").includes(query.toLowerCase());
+  };
 
-// Query search to check against fields and not case insensitive
-const displayData = data.filter(row => {
-  return hasMatch(row.ticket_id.toLowerCase(), query) ||
-         hasMatch(row.initiative.toLowerCase(), query) ||
-         hasMatch(row.description.toLowerCase(), query) ||
-         hasMatch(row.owner.toLowerCase(), query)
-})
+  // Query search to check against fields and not case insensitive
+  const displayData = data.filter((row) => {
+    return (
+      hasMatch(row.ticket_id.toLowerCase(), query) ||
+      hasMatch(row.initiative.toLowerCase(), query) ||
+      hasMatch(row.description.toLowerCase(), query) ||
+      hasMatch(row.owner.toLowerCase(), query)
+    );
+  });
 
-// Handle change when initiative(s) is ticked
-function AddToEstimation () {
-  const [checkboxSelection, setcheckboxSelection] = useState("");
+  // Handle change when initiative(s) is ticked
+  function AddToEstimation() {
+    const [checkboxSelection, setcheckboxSelection] = useState("");
 
-  const addInitiative = () => {
-
+    const addInitiative = () => {};
   }
-}
 
+  return (
+    <div className="container">
+      <Styles>
+        <Container>
+          <InitiativeTopPanel />
+          <div className="searchBar">
+            <div className="textInput">
+              <input
+                type="text"
+                className="searchTerm"
+                placeholder="Search.."
+                onChange={(e) => setQuery(e.target.value)}
+              ></input>
+            </div>
+          </div>
 
-  return <div className="container">
-    <Styles>
-      <Container>
-      <InitiativeTopPanel />
-      <div className="searchBar">
-        <div className="textInput">
-          <input
-            type="text"
-            className="searchTerm"
-            placeholder="Search.."
-            onChange={(e) => setQuery(e.target.value)}
-          ></input>
-        </div>
-      </div>
-
-      <div style={{ height: 650, width: '100%' }}>
-        <DataGrid
-          rows={displayData}
-          getRowId={((obj) => obj._id)}
-          columns={columns}
-          pageSize={10}
-          rowsPerPageOptions={[15]}
-          // checkboxSelection
-          checkboxSelection columns={columns}{...data}  />  
-
-        />
-      </div>
-      <Row className="mb-3">
-        <Col xs={4}>
-          <Button style={{ width: "12rem" }}>Add to Estimation</Button>
-        </Col>
-      </Row>
-      </Container>
-    </Styles>
+          <div style={{ height: 650, width: "100%" }}>
+            <DataGrid
+              rows={displayData}
+              getRowId={(obj) => obj._id}
+              columns={columns}
+              pageSize={10}
+              rowsPerPageOptions={[15]}
+              // checkboxSelection
+              checkboxSelection
+              columns={columns}
+              {...data}
+            />
+            />
+          </div>
+          <Row className="mb-3">
+            <Col xs={4}>
+              <Button style={{ width: "12rem" }}>Add to Estimation</Button>
+            </Col>
+          </Row>
+        </Container>
+      </Styles>
     </div>
+  );
 }
 
 export default Dashboard;
--- src/config/api.js
+++ src/config/api.js
@@ -7,13 +7,13 @@ export default axios.create({
 });
 
 const api = axios.create({
-  // baseURL: "http: localhost:3000",
-  baseURL: "https://roadmap-prioritisation.herokuapp.com/", // Update before deploy to Netlify
+  baseURL: "http://localhost:4000",
+  //baseURL: "https://roadmap-prioritisation.herokuapp.com/", // Update before deploy to Netlify
 });
 
 export const getInitiatives = async () => {
-  const response = await axios.get(
-    "https://roadmap-prioritisation.herokuapp.com/api/v1/initiatives/" //Update before deploy to Netlify
+  const response = await api.get(
+    "/api/v1/initiatives/" //Update before deploy to Netlify
   );
   return response.data;
 };
