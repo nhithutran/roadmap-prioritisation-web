93f6ce678beec5c7ce33f87bb57171744a3bf15a
Nhi Tran <98810653+nhithutran@users.noreply.github.com>
2022-11-18 10:55:39 +1100

    fix InitTopPanel test

--- src/test/EstimationTopPanel.test.js
+++ src/test/EstimationTopPanel.test.js
@@ -1,12 +1,25 @@
-const { EstimationTopPanel } = require('../components/EstimationTopPanel')
+import React from "react"
+import EstimationTopPanel from '../Components/EstimationTopPanel';
+import {render, fireEvent, screen, cleanup} from '@testing-library/react'
+
+let comp = null;
+beforeEach(() => comp = render(<EstimationTopPanel />))
+
+afterEach(cleanup)
 
 describe("EstimationTopPanel", () => {
+
     test('there is a heading', () => {
-        expect('Estimation').toMatch(/Estima/);    
+        expect(comp.queryByText(/Estimation/i)).toBeTruthy();
     }); 
     
     test('to display correct text', () => {
-        expect('The key is not to prioritize what').toContain("not to prioritize what");    
-        expect('Stephen Convey').toContain("Convey");
+        expect(comp.queryByText(/to prioritize what/i)).toBeTruthy();
+         
     });  
+
+    it('should display correct text', () => {
+        expect(comp.queryByText(/Convey/i)).toBeTruthy();
+      });
+
 });
