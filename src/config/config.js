58c7c89daf4e9d8e9a3f278404a07e0a3f56e312
Anthony B Chung <99620815+anthonybchung@users.noreply.github.com>
2022-11-22 20:43:15 +1100

    baseURL

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,13 @@
 import { modalUnstyledClasses } from "@mui/material";
 
 const production = {
-  // baseURL: "https://roadmap-prioritisation.herokuapp.com/", // Update before deploy to Netlify
-  baseURL: "http://localhost:4000/",
+  baseURL: "https://roadmap-prioritisation.herokuapp.com/", // Update before deploy to Netlify
+  //baseURL: "http://localhost:4000/",
 };
 
 const development = {
-  // baseURL: "https://roadmap-prioritisation.herokuapp.com/",
-  baseURL: "http://localhost:4000/",
+  baseURL: "https://roadmap-prioritisation.herokuapp.com/",
+  //baseURL: "http://localhost:4000/",
 };
 
 const config = {
